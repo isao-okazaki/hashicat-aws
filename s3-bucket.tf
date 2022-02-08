b88971c55868496a5cc4d94537ee1545a7805f97
isao.okazaki.yg@hitachi-solutions.com <isao.okazaki.yg@hitachi-solutions.com>
2022-02-08 08:16:43 +0000

    Added s3 bucket module 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.2.0"
 
   bucket = "${var.prefix}-s3-bucket"
   acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
 
 }
